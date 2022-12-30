33d9d17e2ceeca5ab417d3b500988725393033f8
Donovan <donovanriversjr@gmail.com>
2022-12-30 04:31:27 -0500

    Added function to the Adobe API Component

--- src/apiComponents/adobe.js
+++ src/apiComponents/adobe.js
@@ -1,15 +1,49 @@
 import adobe from '../logos/Company=Adobe.svg'
 import './Componentcss/adobe.css';
+import React from 'react';
+import axios from 'axios';
 
 
-function AdobeComp(){
-    return(
-        <div className="App">
-      <header className="App-header">
-        <img src={adobe} className="App-logo" alt="logo" />
-      </header>
-    </div>
-    )
+class AdobeComp extends React.Component{
+  constructor(props){
+    super(props)
+    
+    this.state = {
+      optChain: []
+    }
+  } 
+  componentDidMount(){
+    var options = {
+      method: 'GET',
+      url: 'https://yfapi.net/ws/insights/v1/finance/insights?symbol=ADBE',
+      params: {modules: 'defaultKeyStatistics,assetProfile'},
+      headers: {
+        'x-api-key': 'taaVnR4AqU2jfPSB1JzWM2ZuCJsl3qnw2l8pvKqv'
+      }
+    };
+    axios.request(options)
+    .then(response=>{
+      this.setState({
+          optChain: response.data
+      })
+      console.log(response.data)
+    })
+  }
+  render(){
+    if(!this.state.optChain.finance){
+      return(
+        <h1>Loading</h1>
+      )
+    }
+    if(this.state.optChain.finance){
+      return(
+       <div className="App">
+        <header className="App-header">
+        <img src={adobe} className="App-logo" alt="logo" /> 
+        </header>
+        </div>
+      )}
+  }
 }
 
 export default AdobeComp;
---
+++ src/styleComponents/search.js
@@ -0,0 +1,5 @@
+import magGlass from './images/glass.png'
+
+function SearchBar(){
+    <img src={magGlass} className="" alt="magifying-glass" />
+}
